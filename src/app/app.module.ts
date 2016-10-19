544dc0d5cdb82c96164662015d3441a91988a7ce
Roman Bondarchuk <rmnbnd@gmail.com>
2016-10-19 22:44:31 +0300

    ISSUE-3 - Integrate angular routing and general components - home/login/profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }   from './app.component';
+import { routes }   from './app.routes';
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { ProfileComponent } from "./components/profile/profile.component";
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        ProfileComponent
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+// app.routes.ts
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+export const routes = [{
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+}, {
+    path: 'login',
+    component: LoginComponent
+}, {
+    path: 'profile',
+    component: ProfileComponent
+}];
