124bfe551aa008e696dbd94728b68ca91cd91160
Roman Bondarchuk <rmnbnd@gmail.com>
2016-12-04 22:22:39 +0200

    Setup styles for components (#12)
    * ISSUE-7 - Setup styles in webpack
    
    * ISSUE-7 - Add styles for footer

--- webpack.common.js
+++ webpack.common.js
@@ -29,14 +29,12 @@ module.exports = {
                 loader: 'file?name=assets/[name].[hash].[ext]'
             },
             {
-                test: /\.css$/,
-                exclude: helpers.root('src', 'app'),
-                loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
-            },
-            {
-                test: /\.css$/,
-                include: helpers.root('src', 'app'),
-                loader: 'raw'
+                test: /.css$/,
+                loaders: [
+                    ExtractTextPlugin.extract({fallbackLoader: "style-loader", loader: 'css-loader'}),
+                    'to-string-loader',
+                    'css-loader?sourceMap'
+                ]
             }
         ]
     },
